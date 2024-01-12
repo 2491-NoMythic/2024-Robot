d6f807a0bd2a23115333c2fef6110a399ac17b8d
NoMythic <2491nomythic@gmail.com>
2024-01-11 19:43:46 -0600

    Initial Commit

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,28 +4,71 @@
 
 package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.SparkRelativeEncoder;
+import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import frc.robot.settings.Constants;
 import  frc.robot.settings.Constants.ShooterConstants;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 public class ShooterSubsystem extends SubsystemBase {
   CANSparkMax shooter1;
   CANSparkMax shooter2;
+
   double runSpeed;
-  /** Creates a new Shooter. */
+
+  RelativeEncoder encoder1;
+
+    /** Creates a new Shooter. */
   public ShooterSubsystem(double runSpeed) {
+    SparkPIDController shooterPID;
     shooter1 = new CANSparkMax(ShooterConstants.SHOOTER_1_MOTORID, MotorType.kBrushless);
     shooter2 = new CANSparkMax(ShooterConstants.SHOOTER_2_MOTORID, MotorType.kBrushless);
+    
+    shooter1.restoreFactoryDefaults();
+    shooter2.follow(shooter1);
+    shooter2.setInverted(true);
+    
+
+    encoder1 = shooter1.getEncoder(SparkRelativeEncoder.Type.kQuadrature, 4096);
+
+    shooterPID = shooter1.getPIDController();
+     double kP = Constants.ShooterConstants.kP;
+     double kI = Constants.ShooterConstants.kI;
+     double kD = Constants.ShooterConstants.kD;
+     double kIz = Constants.ShooterConstants.kIz;
+     double kFF = Constants.ShooterConstants.kFF;
+     double kMaxOutput = Constants.ShooterConstants.kMaxOutput;
+     double kMinOutput = Constants.ShooterConstants.kMinOutput;
+
+
+    shooterPID.setP(kP);
+    shooterPID.setI(kI);
+    shooterPID.setD(kD);
+    shooterPID.setIZone(kIz);
+    shooterPID.setFF(kFF);
+    shooterPID.setOutputRange(kMinOutput, kMaxOutput);  
+
+
+    //Adjust the value runSpeed later. (SmartDashboard stuff)
+    shooterPID.setReference(runSpeed, ControlType.kVelocity);
+    
+
+
+
+
   }
   
 
   public void shootThing(double runSpeed) {
     shooter1.set(runSpeed);
-    shooter2.set(-runSpeed);
   }
   public void turnOff(){
     shooter1.set(0);
-    shooter2.set(0);
   }
 }
