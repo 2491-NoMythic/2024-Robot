d8e8ced4a30e96c23fea0d8aff5a5fc5dbcec799
NoMythic <2491nomythic@gmail.com>
2024-01-16 17:34:05 -0600

    AAAAAA

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -15,7 +15,8 @@
  import edu.wpi.first.math.controller.PIDController;
  import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  import edu.wpi.first.wpilibj2.command.SubsystemBase;
- 
+ import edu.wpi.first.hal.can.CANStreamOverflowException;
+ import edu.wpi.first.math.geometry.Pose2d;
  
  public class ShooterSubsystem extends SubsystemBase {
    CANSparkMax shooter1;
