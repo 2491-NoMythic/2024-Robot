dccd2cc435ff1c2beec460a39bd5fb2c0e07f4cd
2491NoMythic <2491nomythic@gmail.com>
2024-02-06 21:31:27 -0600

    changed order of things done in teleopInit()

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -79,10 +79,10 @@ public class Robot extends TimedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
-    m_robotContainer.teleopInit();
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+    m_robotContainer.teleopInit();
   }
 
   /** This function is called periodically during operator control. */
