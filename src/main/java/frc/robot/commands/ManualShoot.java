9868102c33e75e29661b3b283f193890abcbfd0f
Rowan Flood <121908273+rflood07@users.noreply.github.com>
2024-02-12 22:06:42 -0600

    switched import in ManualShoot

--- src/main/java/frc/robot/commands/ManualShoot.java
+++ src/main/java/frc/robot/commands/ManualShoot.java
@@ -4,7 +4,7 @@
 
 package frc.robot.commands;
 import frc.robot.settings.Constants.ShooterConstants;
-import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.IndexerSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class ManualShoot extends Command {
