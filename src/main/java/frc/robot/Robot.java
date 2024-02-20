000378272bb8ba1e1272be8d36144ac320039548
Rowan Flood <121908273+rflood07@users.noreply.github.com>
2024-02-19 18:29:54 -0600

    removed preferences setting

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -29,11 +29,6 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-    Preferences.setBoolean("Intake", false);
-    Preferences.setBoolean("Indexer", false);
-    Preferences.setBoolean("Shooter", false);
-    Preferences.setBoolean("AngleShooter", true);
-    Preferences.setBoolean("Climber", false);
     m_robotContainer = new RobotContainer();
   }
 
